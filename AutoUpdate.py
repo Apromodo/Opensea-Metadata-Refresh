e4c65b10c40ed864e432fcc05eca296c1952dd89
Kaya Ünal <kayaunal92@gmail.com>
2021-11-22 17:28:26 +0300

    Update AutoUpdate.py

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -3,36 +3,32 @@ import time
 
 # change NFT Collection adres and change the range
 url = "https://api.opensea.io/asset/0xfce5e89772359843521d18fcc958eb923842fdd3/"
-update_flag = "/?force_update=true"
+UpdtFlag = "/?force_update=true"
 
-ids = [ i for i in range(1, 1000)]
-
-for i in ids:
-  req_url = url + str(i) + update_flag
-  r = requests.get(req_url)
+reqUn = [ i for i in range(1, 1000)]
+for i in reqUn:
+  r_url = url + str(i) + UpdtFlag
+  r = requests.get(r_url)
   print(i, r.status_code)
-  time.sleep(0.1)
+  time.sleep(0)
   
-ids = [ i for i in range(1, 1000)]
-
-for i in ids:
-  req_url = url + str(i) + update_flag
-  r = requests.get(req_url)
+reqUn = [ i for i in range(1, 1000)]
+for i in reqUn:
+  r_url = url + str(i) + UpdtFlag
+  r = requests.get(r_url)
   print(i, r.status_code)
-  time.sleep(0.1)
+  time.sleep(0)
   
-ids = [ i for i in range(1, 1000)]
-
-for i in ids:
-  req_url = url + str(i) + update_flag
-  r = requests.get(req_url)
+reqUn = [ i for i in range(1, 1000)]
+for i in reqUn:
+  r_url = url + str(i) + UpdtFlag
+  r = requests.get(r_url)
   print(i, r.status_code)
-  time.sleep(0.1)
+  time.sleep(0)
 
-  ids = [ i for i in range(1, 1000)]
-
-for i in ids:
-  req_url = url + str(i) + update_flag
-  r = requests.get(req_url)
+reqUn = [ i for i in range(1, 1000)]
+for i in reqUn:
+  r_url = url + str(i) + UpdtFlag
+  r = requests.get(r_url)
   print(i, r.status_code)
-  time.sleep(0.1)
+  time.sleep(0)
